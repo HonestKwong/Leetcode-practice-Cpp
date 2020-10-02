57b571863621cef1b2fb89533a47639f32e3e116
Kwong <aukhheng@mail.scut.edu.cn>
2020-10-03 01:00:01 +0800

    10.2practice

--- 剑指 Offer 33. 二叉搜索树的后序遍历序列.cpp
+++ 剑指 Offer 33. 二叉搜索树的后序遍历序列.cpp
@@ -16,3 +16,22 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    bool verifyPostorder(vector<int>& postorder) {
+        stack<int> tmp_stack;
+        int root = INT_MAX;
+        for(int i = postorder.size()-1; i>=0; i--){
+            if(postorder[i]>root) return false;
+            while(!tmp_stack.empty()&&(postorder[i]<tmp_stack.top())){
+                root = tmp_stack.top();
+                tmp_stack.pop();
+            }
+
+            tmp_stack.push(postorder[i]);
+        }
+        return true;
+    }
+};
